22ab6b296d1e159cedeb90dc37a78870788e7529
hackboxlive <singhsarvshakti127@gmail.com>
2018-02-23 03:16:38 +0530

    Buffering dataset

--- dumber_db.py
+++ dumber_db.py
@@ -9,5 +9,40 @@ connection = sqlite3.connect('{}.db'.format(timeframe))
 ptr = connection.cursor()
 
 def create_table():
-    ptr.execute("""CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)""")
-    
+	ptr.execute("""CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)""")
+
+def format_data(data):
+	data = data.replace("\n"," newlinechar ")
+	data = data.replace("\r", " newlinechar ")
+	data = data.replace('"',"'")
+	return data
+	
+def find_parent(pid):
+	try:
+		sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
+		c.execute(sql)
+		result = c.fetchone()
+		if result != None:
+			return result[0]
+		else:
+			return False
+	except Exception as e:
+		#print("find_parent",e)
+		return False
+
+if __name__ == "__main__":
+	create_table()
+	row_counter = 0
+	paired_rows = 0
+	data_path = ""
+	with open(data_path, buffer = 1000) as f:
+		for row in f:
+			row_counter += 1
+			row = json.loads(row)
+			parent_id = row['parent_id']
+			body = format_data(row['body'])
+			created_utc = row['created_utc']
+			score = row['score']
+			subreddit = row['subreddit']
+
+			parent_data = find_parent(parent_id)
